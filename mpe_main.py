6b2aafb36df036114dd91840ebb66fb16bf20a0d
hong-sh <rkskekfk2020@naver.com>
2021-12-30 15:37:52 +0900

    main file changed

--- mpe_main.py
+++ mpe_main.py
@@ -1,20 +1,25 @@
 import supersuit
 import pettingzoo.mpe.simple_tag_v2 as simple_tag_v2
+import gym
 import random
 import numpy as np
+
+import algorithm.ppo_agent as ppo
 
 def get_action():
     return np.random.randint(0,5)
 
 if __name__ == "__main__":
     env = simple_tag_v2.env(num_good=3, num_adversaries=3, num_obstacles=2, max_cycles=25, continuous_actions=False)
+    env = gym.make('CartPole-v1')
+    agent = ppo(env.observation_space.shape[0], env.action_space.shape[0])
 
     env.reset()
     step_cnt = 0
     for _ in range(1000):
         step_cnt += 1
         print('step cnt : {}'.format(step_cnt))
-        action = get_action()
+        action = agent.get_action()
         env.step(action)
         next_state, reward, done, info = env.last()
         env.render()
