a113187be158113b688ea10a58b3baa6f5570d2d
hong-sh <rkskekfk2020@naver.com>
2022-01-13 09:09:14 +0900

    mpe + ppo update

--- mpe_main.py
+++ mpe_main.py
@@ -4,25 +4,39 @@ import gym
 import random
 import numpy as np
 
-import agent.ppo_agent as ppo
+from agent.ppo_agent import PPOAgent
 
 def get_action():
     return np.random.randint(0,5)
 
+env_config = {
+    "num_good" : 3,
+    "num_adversaries" : 3,
+    "num_obstacles" : 2,
+    "max_cycles" : 25,
+    "continuous_actions" : False
+}
 
 
 if __name__ == "__main__":
-    env = simple_tag_v2.env(num_good=3, num_adversaries=3, num_obstacles=2, max_cycles=25, continuous_actions=False)
-    env = gym.make('CartPole-v1')
-    agent = ppo(env.observation_space.shape[0], env.action_space.shape[0])
+    env = simple_tag_v2.env(num_good=env_config["num_good"], num_adversaries=env_config["num_adversaries"], \
+        num_obstacles=env_config["num_obstacles"], max_cycles=env_config["max_cycles"], \
+            continuous_actions=env_config["continuous_actions"])
 
-    env.reset()
-    step_cnt = 0
-    for _ in range(1000):
-        step_cnt += 1
-        print('step cnt : {}'.format(step_cnt))
-        action = agent.get_action()
-        env.step(action)
-        next_state, reward, done, info = env.last()
-        env.render()
-        print('next_state : {}, reward : {}, done : {}, info : {}'.format(next_state, reward, done, info))
+    agent = PPOAgent(14, 5)
+
+    for i_eps in range(10000):
+        env.reset()
+        state = env.last()
+        print(state)
+        print(np.asarray(state[0]).shape)
+        step_cnt = 0
+
+        for _ in range(1000):
+            step_cnt += 1
+            print('step cnt : {}'.format(step_cnt))
+            action = agent.get_action()
+            env.step(action)
+            next_state, reward, done, info = env.last()
+            env.render()
+            print('next_state : {}, reward : {}, done : {}, info : {}'.format(next_state, reward, done, info))
