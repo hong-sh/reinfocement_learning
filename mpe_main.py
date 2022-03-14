822128bcb2c84453fec9fde0c7865537982842ca
hong-sh <rkskekfk2020@naver.com>
2022-03-14 21:13:15 +0900

    mpe tensorboard commit

--- mpe_main.py
+++ mpe_main.py
@@ -1,8 +1,11 @@
+from re import A
 import supersuit
 import pettingzoo.mpe.simple_tag_v2 as simple_tag_v2
 import gym
 import random
 import numpy as np
+from time import time_ns
+from tensorboardX import SummaryWriter
 
 from agent.ppo_agent import PPOAgent
 
@@ -13,7 +16,7 @@ env_config = {
     "num_good" : 2,
     "num_adversaries" : 3,
     "num_obstacles" : 2,
-    "max_cycles" : 100,
+    "max_cycles" : 1000,
     "continuous_actions" : False
 }
 
@@ -29,6 +32,8 @@ if __name__ == "__main__":
     adversary_agent = PPOAgent(adversary_observation , 5)
     good_agent = PPOAgent(good_observation, 5)
 
+    summary_writer = SummaryWriter('logs/mpe_main_' + str(time_ns()))
+
     for i_eps in range(10000):
         env.reset()
         prev_state = [np.zeros(adversary_observation) for _ in range(env_config["num_adversaries"])]
@@ -40,11 +45,13 @@ if __name__ == "__main__":
             next_state, reward, done, info = env.last()
             if not done:
                 # print('step cnt : {}'.format(step_cnt))
-                # print('next_state : {}, reward : {}, done : {}, info : {}'.format(next_state, reward, done, info))
                 action = 0
                 if agent_idx < env_config["num_adversaries"]:
                     action, action_prob = adversary_agent.get_action(next_state)
+                    print('agent_idx : {}, prev_state : {}'.format(agent_idx, prev_state[agent_idx])                     )
+                    print('action : {}, next_state : {}, reward : {}, done : {}, info : {}'.format(action, next_state, reward, done, info))
                     adversary_agent.save_xp((prev_state[agent_idx], next_state, action, action_prob[action].item(), reward, done))
+                    # TODO fix wrong cycles 
                     prev_state[agent_idx] = next_state
                     sum_reward += reward
                 elif agent_idx >= env_config["num_adversaries"]:
@@ -53,5 +60,10 @@ if __name__ == "__main__":
                 env.step(action)
                 # env.render()
             step_cnt += 1
-        adversary_agent.train()
+        pi_loss, value_loss, td_error =  adversary_agent.train()
+        summary_writer.add_scalar('Loss/pi_loss', pi_loss, i_eps)
+        summary_writer.add_scalar('Loss/value_loss', value_loss, i_eps)
+        summary_writer.add_scalar('Loss/td_error', td_error, i_eps)
+        summary_writer.add_scalar('Episode reward', sum_reward, i_eps)
         print('{} eps total reward : {}'.format(i_eps, sum_reward))
+
